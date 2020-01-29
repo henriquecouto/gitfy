5d3c8c16af1c3946b187c6a551903d2a4d63bbea
Henrique Couto <henrique.coutu@live.com>
2020-01-29 20:53:17 -0300

    drawer added

--- src/screens/Header/index.js
+++ src/screens/Header/index.js
@@ -4,19 +4,48 @@ import {
   Toolbar,
   IconButton,
   AppBar,
-  Grid
+  Grid,
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Divider,
+  CssBaseline
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import {
   Menu as MenuIcon,
   Brightness4 as Brightness4Icon,
-  Brightness7 as Brightness7Icon
+  Brightness7 as Brightness7Icon,
+  Inbox as InboxIcon,
+  Mail as MailIcon
 } from "@material-ui/icons";
 
+const drawerWidth = 240;
+
 const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex"
+  },
   menuButton: {
     marginRight: theme.spacing(2)
-  }
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0
+  },
+  drawerPaper: {
+    width: drawerWidth
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3)
+  },
+  toolbar: theme.mixins.toolbar
 }));
 
 export default function Header({
@@ -28,8 +57,9 @@ export default function Header({
   const classes = useStyles();
 
   return (
-    <div>
-      <AppBar position="static" color="default">
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" color="default" className={classes.appBar}>
         <Toolbar>
           <Grid container justify="space-between" alignItems="center">
             <Grid item>
@@ -57,7 +87,40 @@ export default function Header({
           </Grid>
         </Toolbar>
       </AppBar>
-      {children}
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper
+        }}
+      >
+        <div className={classes.toolbar} />
+        <List>
+          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {["All mail", "Trash", "Spam"].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        {children}
+      </main>
     </div>
   );
 }
