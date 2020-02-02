9093d53a044ca922e99b0f8b734f5ddfa4e8b4a1
Henrique Couto <henrique.coutu@live.com>
2020-02-02 19:40:16 -0300

    reading more than one files in a commit

--- src/screens/CommitView/index.js
+++ src/screens/CommitView/index.js
@@ -1,12 +1,44 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { loadCommit, loadBranch } from "../../services/db";
-import { Grid, Typography } from "@material-ui/core";
+import { Grid, Typography, Divider, Paper, Hidden } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import Markdown from "react-markdown";
+
+const useStyles = makeStyles(theme => ({
+  fileGrid: {
+    padding: theme.spacing(2)
+  },
+  filePaper: {
+    margin: theme.spacing(1, 0)
+  }
+}));
+
+const PaperFile = ({ file }) => {
+  const classes = useStyles();
+  return (
+    <Paper className={classes.filePaper}>
+      <Grid
+        container
+        className={classes.fileGrid}
+        direction="column"
+        spacing={2}
+      >
+        <Grid item>
+          <Typography variant="h6">Arquivo: {file.path}</Typography>
+        </Grid>
+        <Divider variant="fullWidth" />
+        <Grid item>
+          <Markdown source={file.doc} escapeHtml={false} />
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+};
 
 export default function CommitView({ setPosition }) {
   const { commitId } = useParams();
-  const [commit, setCommit] = useState({});
+  const [commit, setCommit] = useState(null);
   const [branch, setBranch] = useState({});
 
   useEffect(() => {
@@ -27,20 +59,35 @@ export default function CommitView({ setPosition }) {
 
   return (
     !!commit && (
-      <Grid container justify="center" direction="column">
+      <Grid container justify="center" direction="column" spacing={4}>
         <Grid item>
           <Grid container justify="space-between">
             <Grid item>
-              <Typography>Arquivo: {commit.filePath}</Typography>
+              <Typography variant="h5">Descrição: {commit.desc}</Typography>
             </Grid>
             <Grid item>
-              <Typography>Branch: {branch.name}</Typography>
+              <Typography variant="h5">Branch: {branch.name}</Typography>
             </Grid>
             <Grid item></Grid>
           </Grid>
         </Grid>
         <Grid item>
-          <Markdown source={commit.doc} escapeHtml={false} />
+          <Grid container spacing={2}>
+            {commit.files.map((v, i) => (
+              <React.Fragment key={i}>
+                <Hidden mdDown>
+                  <Grid item xs={4} key={i}>
+                    <PaperFile file={v} />
+                  </Grid>
+                </Hidden>
+                <Hidden lgUp>
+                  <Grid item xs={12}>
+                    <PaperFile file={v} />
+                  </Grid>
+                </Hidden>
+              </React.Fragment>
+            ))}
+          </Grid>
         </Grid>
       </Grid>
     )
--- src/screens/Commits/FileCard.js
+++ src/screens/Commits/FileCard.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Paper, Grid, Divider, ButtonGroup, Button } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import Markdown from "react-markdown";
